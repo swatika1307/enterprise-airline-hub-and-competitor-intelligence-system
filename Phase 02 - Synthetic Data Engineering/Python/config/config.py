d00c1f1a2b7ab83c1190e51fbd09847b3f6feb0d
Swatika <83584049+swatika1307@users.noreply.github.com>
2026-07-22 19:31:49 +0530

    config.py file

---
+++ Phase 02 - Synthetic Data Engineering/Python/config/config.py
@@ -0,0 +1,28 @@
+'''
+Project Configuration
+
+Date Started - 15.07.2026
+Last Modified - 15.07.2026
+'''
+
+# ------------
+# RANDOM SEED
+# ------------
+RANDOM_SEED = 42
+
+# -----------------
+# PROJECT SETTINGS
+# -----------------
+PROJECT_NAME = "Enterprise Airline Hub & Competitor Intelligence System"
+PROJECT_YEAR = 2026
+NUMBER_OF_AIRLINES = 6
+SIMULATION_DAYS = 365
+LOAD_FACTOR = 0.83
+CURRENCY = "INR"
+
+# -------------------------
+# DATA GENERATION SETTINGS
+# -------------------------
+GENERATE_DOMESTIC = True
+GENERATE_INTERNATIONAL = True
+EXPORT_CSV = True
